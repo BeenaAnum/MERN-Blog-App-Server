50fa8e5062ee2725bf54e3d8416e63211ba1bc77
BeenaAnum <beenaanum@gmail.com>
2026-05-23 22:45:03 +0500

    Configure MERN app production deployment builds

--- server.js
+++ server.js
@@ -145,3 +145,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+// At the bottom of server.js / app.js
+module.exports = app;
